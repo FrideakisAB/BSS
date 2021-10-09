5158e83cfa6bf1e88022055d776aa414b2ce73c4
FrideakisAB <reterdastion@gmail.com>
2021-10-09 10:02:36 +0300

    Была создана таблица стилей\nРеализовано главное меню\nНачата реализация формы CustomerMenu

---
+++ BSS/CustomerMenu.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BSS
+{
+    /// <summary>
+    /// Логика взаимодействия для CustomerMenu.xaml
+    /// </summary>
+    public partial class CustomerMenu : Page
+    {
+        public CustomerMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
